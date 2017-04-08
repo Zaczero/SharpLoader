c99e2bf9194cce7583dea242f647e5bcd0f3c7f1
Zaczero <kamil.monicz@wp.pl>
2017-04-08 23:58:08 +0200

    Added manifest file, changed file title

--- SharpLoader/Properties/AssemblyInfo.cs
+++ SharpLoader/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SharpLoader")]
+[assembly: AssemblyTitle("[C#] Source code randomizer and compiler")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Zaczero")]
